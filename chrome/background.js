6131475a1ca14fffdf5aac602dcb2a4c1df7cd69
bowmanhan <fancycode@gmail.com>
2013-11-20 14:33:29 +0800

    reoriganite

--- chrome/background.js
+++ chrome/background.js
@@ -1,6 +1,9 @@
 
 // http://heuet.tsk.erya100.com/flash/videoplay/flash_main.js?t=201204060226
-
+ var Counter = (function(){
+				var i = 0;
+				return function(){ return ++i; }
+			})();
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
@@ -8,6 +11,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     // Redirect the lolcal request to a random loldog URL.
 	if(info.url.match(/flash\_main\.js/) ){
 		console.log("url redirected: " + info.url);
+		Counter();
 		return {redirectUrl:"http://rawgithub.com/bugparty/erya_crack/master/flash_main.js" };
 		}
 		
@@ -22,7 +26,3 @@ chrome.webRequest.onBeforeRequest.addListener(
   ["blocking"]
   );
  
- var Clicks = (function(){
-				var i = 0;
-				return function(){ return ++i; }
-			})();
